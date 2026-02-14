d0735a811290b534bf9e999810a61a1ac7b0fd53
Mukit Rahman <110609965+mukit-rahman1@users.noreply.github.com>
2026-02-14 12:45:35 -0500

    Update stats.h

--- include/stats.h
+++ include/stats.h
@@ -5,8 +5,8 @@
 struct Stats {
   uint64_t samples = 0;
   uint64_t misses = 0;
-  int64_t min_jitter_ns = std::numeric_limits<int64_t>::max();
-  int64_t max_jitter_ns = std::numeric_limits<int64_t>::min();
+  int64_t min_jitter_ns = std::numeric_limits<int64_t>::max(); //init smallest jitter so far as the biggest signed int
+  int64_t max_jitter_ns = std::numeric_limits<int64_t>::min(); //init biggest jitter so far as the smallest signed int
   long double sum_jitter_ns = 0.0L;
 
   void add(int64_t jitter_ns, int64_t miss_threshold_ns) {
@@ -22,12 +22,12 @@ struct Stats {
   }
 
   int64_t min_or0() const {
-    return (min_jitter_ns == std::numeric_limits<int64_t>::max()) ? 0 : min_jitter_ns;
+    return (min_jitter_ns == std::numeric_limits<int64_t>::max()) ? 0 : min_jitter_ns; //if nothing yet ret 0
   }
 
   int64_t max_or0() const {
-    return (max_jitter_ns == std::numeric_limits<int64_t>::min()) ? 0 : max_jitter_ns;
+    return (max_jitter_ns == std::numeric_limits<int64_t>::min()) ? 0 : max_jitter_ns;//if nothing yet ret 0
   }
 
-  void reset() { *this = Stats{}; }
+  void reset() { *this = Stats{}; }//shortcut to reset. Makes new Stats obj and points to it.
 };
