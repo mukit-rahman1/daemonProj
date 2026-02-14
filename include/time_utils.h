34ad29e5aa8fa3480fb669e0eab497eaf85ccea4
Mukit Rahman <110609965+mukit-rahman1@users.noreply.github.com>
2026-02-14 12:50:35 -0500

    Update time_utils.h Comments and Readability

--- include/time_utils.h
+++ include/time_utils.h
@@ -1,11 +1,15 @@
 #pragma once
-#include <cstdint>
-#include <ctime>
+#include <cstdint> //int64_t
+#include <ctime> //timespec 
 
+//helpers for timespec as monotonic clock expects it
+
+//convert timsepec to ns
 static inline int64_t ts_to_ns(const timespec& ts) {
   return int64_t(ts.tv_sec) * 1000000000LL + int64_t(ts.tv_nsec);
 }
 
+//convert ns to timespec
 static inline timespec ns_to_ts(int64_t ns) {
   timespec ts{};
   ts.tv_sec = time_t(ns / 1000000000LL);
@@ -13,6 +17,7 @@ static inline timespec ns_to_ts(int64_t ns) {
   return ts;
 }
 
+//add ns to the timespec
 static inline timespec add_ns(const timespec& a, int64_t add) {
   return ns_to_ts(ts_to_ns(a) + add);
 }
