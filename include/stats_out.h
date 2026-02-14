0186b90c1a8815dc7c2ffb62d8c1e39de795e883
Mukit Rahman <110609965+mukit-rahman1@users.noreply.github.com>
2026-02-14 13:14:47 -0500

    Update stats_out.h for Comments and Readability

--- include/stats_out.h
+++ include/stats_out.h
@@ -1,19 +1,20 @@
 #pragma once
 #include <cstdio>
 #include <string>
-#include <cerrno>
-#include <cstring>
-#include <sys/stat.h>
+#include <cerrno> //err numbers
+#include <cstring> //std::strerror. err msgs
+#include <sys/stat.h> //for chmod
 #include "rt_config.h"
 #include "stats.h"
 
+//helper function to print stats into .stats file
 static inline void write_stats_out(const Config& cfg, const Stats& st) {
   if (cfg.stats_out.empty()) return;
 
   const std::string tmp = cfg.stats_out + ".tmp";
   FILE* f = std::fopen(tmp.c_str(), "w");
   if (!f) {
-    std::fprintf(stderr, "rtmond: stats_out fopen('%s') failed: %s\n",
+    std::fprintf(stderr, "rtmonitord: stats_out fopen('%s') failed: %s\n",
                  tmp.c_str(), std::strerror(errno));
     return;
   }
@@ -34,10 +35,11 @@ static inline void write_stats_out(const Config& cfg, const Stats& st) {
   std::fflush(f);
   std::fclose(f);
 
-  ::chmod(tmp.c_str(), 0644);
+  ::chmod(tmp.c_str(), 0644); //owner can read and write. Everyone else can read, no one else can write
 
+  //replace and update old file
   if (::rename(tmp.c_str(), cfg.stats_out.c_str()) != 0) {
-    std::fprintf(stderr, "rtmond: rename('%s'->'%s') failed: %s\n",
+    std::fprintf(stderr, "rtmonitord: rename('%s'->'%s') failed: %s\n",
                  tmp.c_str(), cfg.stats_out.c_str(), std::strerror(errno));
   }
 }
